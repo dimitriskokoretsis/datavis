bbb209c5f35ec7fad3480404ac47b4bf8815d0a4
dimitriskokoretsis <dkokoret@gmail.com>
2022-05-09 21:34:51 +0300

    Transfer hidden functions to unhidden R files, to make them usable.

--- R/.get_dodge_width.R
+++ R/.get_dodge_width.R
@@ -1,10 +1,1 @@
-.get_dodge_width <- function(plot) {
 
-  if(!is.null(plot$layers[[1]]$position$width)) {
-    dodge.width <- plot$layers[[1]]$position$width
-  } else {
-    dodge.width <- plot$layers[[1]]$position$dodge.width
-  }
-
-  return(dodge.width)
-}
